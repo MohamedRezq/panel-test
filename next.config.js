234979ddfe0f9c78507f801d0a27680b77fc5582
MohamedRezq <mrezq.dev@gmail.com>
2023-11-02 09:09:31 +0200

    removed env from next.config.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   images: {
     domains: ["todoorstep.s3.amazonaws.com", "images.todoorstep.com"],
   },
-  env: {
-    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
-    NEXT_PUBLIC_NEXTAUTH_SECRET: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
-    NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
-    __NEXT_PRIVATE_PREBUNDLED_REACT:
-      process.env.__NEXT_PRIVATE_PREBUNDLED_REACT,
-  },
 };
 
 module.exports = nextConfig;
